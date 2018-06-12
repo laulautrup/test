d10031f2fd338aa7f9bbef60637ddec60df48a6a
Lautrup Lau <selautrupl@sec02w230yhtdf-lau.local>
2018-06-12 09:51:15 +0200

    Initial Commit

---
+++ SwinjectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwinjectTest
+//
+//  Created by Lautrup Lau on 2018-06-12.
+//  Copyright © 2018 Lautrup Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
